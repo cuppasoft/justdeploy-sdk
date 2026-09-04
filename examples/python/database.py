a041a5a0934a88cb4b3aff8afdaa11561df5c4f4
whitesoil <whitesoil.kr@gmail.com>
2026-09-05 02:08:42 +0900

    style: Python DB 예제의 포맷 검사를 맞춘다

--- examples/python/database.py
+++ examples/python/database.py
@@ -1,6 +1,5 @@
 from justdeploy import JustDeploy
 
 with JustDeploy() as justdeploy:
-    justdeploy.databases.query("your-database-id",
-        "SELECT * FROM orders WHERE customer = ?", params=["Ada"])
+    justdeploy.databases.query("your-database-id", "SELECT * FROM orders WHERE customer = ?", params=["Ada"])
     print("query completed")
